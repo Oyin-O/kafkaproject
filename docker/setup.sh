8a55527f6cf396a24ca631563c103bc02561255d
Oyin-O <olapadeoyindamola@gmail.com>
2025-03-11 23:55:28 +0000

    Cleaned the directory

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,15 +1,15 @@
 TOPIC_NAME="fraud_transactions"
 BROKER="localhost:9092"
-KAFKA_CONTAINER="kafka"  # Update this if your Kafka container has a different name
+KAFKA_CONTAINER="kafka"
 
-echo "⏳ Waiting for Kafka to be ready..."
-sleep 10  # Give Kafka some time to start
+echo "Waiting for Kafka to be ready..."
+sleep 10
 
-echo "🚀 Creating Kafka topic: $TOPIC_NAME..."
+echo "Creating Kafka topic: $TOPIC_NAME..."
 docker exec -it $KAFKA_CONTAINER kafka-topics \
   --create --topic $TOPIC_NAME \
   --bootstrap-server $BROKER \
   --partitions 1 \
   --replication-factor 1
 
-echo "✅ Kafka topic '$TOPIC_NAME' created!"
+echo "Kafka topic '$TOPIC_NAME' created!"
